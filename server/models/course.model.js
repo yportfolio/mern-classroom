a76ddde1dd559f7fbe75e6e47fcf5262b14f3772
wangyes <wangyes@yonyou.com>
2021-07-19 20:37:59 +0800

    Create the course model

---
+++ server/models/course.model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const LessonSchema = new mongoose.Schema({
+  title: String,
+  content: String,
+  resource_url: String,
+});
+const lesson = mongoose.model("Lesson", LessonSchema);
+
+const CourseSchema = new mongoose.Schema({
+  name: { type: String, trim: true, required: "Name is required" },
+  image: { data: Buffer, contentType: String },
+  description: { type: String, trim: true },
+  category: { type: String, trim: true },
+  updated: Date,
+  created: { type: Date, default: Date.now },
+  instructor: { type: mongoose.Schema.ObjectId, ref: "User" },
+  published: { type: Boolean, default: false },
+  lessons: [LessonSchema],
+});
+
+export default mongoose.model("Course", CourseSchema);
